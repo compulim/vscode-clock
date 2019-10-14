0dd8ab4bbabfb903923b10ad950c2a305e780b37
Shreya Dahal <shreyadahal@gmail.com>
2019-10-14 16:24:12 -0400

    Check configuration to set clock's alignment

--- clockstatusbaritem.js
+++ clockstatusbaritem.js
@@ -6,10 +6,33 @@ const
   vscode = require('vscode');
 
 const DEFAULT_DATE_FORMAT = 'hh:MM TT';
+const DEFAULT_CLOCK_ALIGNMENT = vscode.StatusBarAlignment.Right;
+
+function clockAlignment() {
+  const confAlignment = vscode.workspace.getConfiguration('clock').alignment || '';
+
+  switch (confAlignment.toLowerCase()) {
+    case 'left':
+      return vscode.StatusBarAlignment.Left;
+    case 'right':
+      return vscode.StatusBarAlignment.Right;
+    default:
+      return DEFAULT_CLOCK_ALIGNMENT;
+  }
+}
+
+function clockPriority() {
+  switch (clockAlignment()) {
+    case vscode.StatusBarAlignment.Left:
+      return 100;
+    case vscode.StatusBarAlignment.Right:
+      return -100;
+  }
+}
 
 class StatusBarItem {
   constructor() {
-    this._statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, -100);
+    this._statusBarItem = vscode.window.createStatusBarItem(clockAlignment(), clockPriority());
     this._statusBarItem.command = 'clock.insertDateTime';
     this._statusBarItem.tooltip = 'Click to insert into selection';
     this._statusBarItem.show();
