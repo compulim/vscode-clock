246aea851cea09cb194e74cb609d45f503041b88
Shreya Dahal <shreyadahal@gmail.com>
2019-10-14 20:04:01 -0400

    Use getters to get alignment and priority values

--- clockstatusbaritem.js
+++ clockstatusbaritem.js
@@ -8,28 +8,6 @@ const
 const DEFAULT_DATE_FORMAT = 'hh:MM TT';
 const DEFAULT_CLOCK_ALIGNMENT = vscode.StatusBarAlignment.Right;
 
-function clockAlignment() {
-  const confAlignment = vscode.workspace.getConfiguration('clock').alignment || '';
-
-  switch (confAlignment.toLowerCase()) {
-    case 'left':
-      return vscode.StatusBarAlignment.Left;
-    case 'right':
-      return vscode.StatusBarAlignment.Right;
-    default:
-      return DEFAULT_CLOCK_ALIGNMENT;
-  }
-}
-
-function clockPriority() {
-  switch (clockAlignment()) {
-    case vscode.StatusBarAlignment.Left:
-      return 100;
-    case vscode.StatusBarAlignment.Right:
-      return -100;
-  }
-}
-
 class StatusBarItem {
   constructor() {
     this._statusBarItem = this._createStatusBar();
@@ -40,8 +18,30 @@ class StatusBarItem {
     this.refreshUI();
   }
 
+  get _clockAlignment() {
+    const confAlignment = vscode.workspace.getConfiguration('clock').alignment || '';
+
+    switch (confAlignment.toLowerCase()) {
+      case 'left':
+        return vscode.StatusBarAlignment.Left;
+      case 'right':
+        return vscode.StatusBarAlignment.Right;
+      default:
+        return DEFAULT_CLOCK_ALIGNMENT;
+    }
+  }
+
+  get _clockPriority() {
+    switch (this._clockAlignment) {
+      case vscode.StatusBarAlignment.Left:
+        return 100;
+      case vscode.StatusBarAlignment.Right:
+        return -100;
+    }
+  }
+
   _createStatusBar() {
-    const _statusBarItem = vscode.window.createStatusBarItem(clockAlignment(), clockPriority());
+    const _statusBarItem = vscode.window.createStatusBarItem(this._clockAlignment, this._clockPriority);
     _statusBarItem.command = 'clock.insertDateTime';
     _statusBarItem.tooltip = 'Click to insert into selection';
     _statusBarItem.show();
